cea403a7217638c62080d68035ae4c3c52ac1f42
Patrick Ineichen <github@frokonet.ch>
2021-03-14 15:40:24 +0100

    feat: 🎸 Add splash screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
+import SplashScreen from '../components/splash-screen';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
@@ -10,8 +12,22 @@ import Media from './media';
 import Shop from './shop';
 import Contact from './contact';
 
-export default function Home() {
-  return (
+const Home = () => {
+  const [isLoading, setLoading] = useState(true);
+
+  const MotionSplashScreen = motion(SplashScreen);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
+  return isLoading ? (
+    <AnimatePresence>
+      <MotionSplashScreen initial={{ opacity: 1 }} transition={{ duration: 1 }} exit={{ opacity: 0 }} />
+    </AnimatePresence>
+  ) : (
     <Layout>
       <SEO title="PGL" />
       <News />
@@ -22,4 +38,6 @@ export default function Home() {
       <Contact />
     </Layout>
   );
-}
+};
+
+export default Home;
