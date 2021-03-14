634859186754dcc4f8122ea456f7b094e1f069fd
Patrick Ineichen <github@frokonet.ch>
2021-03-14 14:22:58 +0100

    feat: 🎸 Add header to template pages

--- src/templates/template-layout.js
+++ src/templates/template-layout.js
@@ -5,23 +5,21 @@ import tw, { GlobalStyles } from 'twin.macro';
 
 import 'typeface-inter';
 import '../styles/style.css';
+import Header from '../components/header';
 
 const Layout = ({ children }) => {
   const PageContainer = tw.div`min-h-screen overflow-y-auto overflow-x-hidden`;
   const Content = tw.div`mt-4 `;
 
-  const goBack = () => {
-    navigate('/');
-  };
-
   return (
     <>
       <GlobalStyles />
       <PageContainer>
-        <div className="relative container mx-auto mt-8 h-72 border-b border-pgl-blue">
+        <Header />
+        <div className="relative container mx-auto mt-20 h-72 border-b border-pgl-blue">
           <button
             type="button"
-            onClick={() => goBack()}
+            onClick={() => navigate('/')}
             class="absolute left-0 bottom-4 items-center ml-4 md:ml-0 px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-pgl-blue hover:bg-blue-800"
           >
             Zurück
