4a5cd4485ac19551d141f0fb70bb1cd7d46bae0e
Patrick Ineichen <github@frokonet.ch>
2021-03-14 17:37:22 +0100

    feat: 🎸 Remove splash screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
+import React from 'react';
 
-import SplashScreen from '../components/splash-screen';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
@@ -13,21 +11,7 @@ import Shop from './shop';
 import Contact from './contact';
 
 const Home = () => {
-  const [isLoading, setLoading] = useState(true);
-
-  const MotionSplashScreen = motion(SplashScreen);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 5000);
-  }, []);
-
-  return isLoading ? (
-    <AnimatePresence>
-      <MotionSplashScreen initial={{ opacity: 1 }} transition={{ duration: 1 }} exit={{ opacity: 0 }} />
-    </AnimatePresence>
-  ) : (
+  return (
     <Layout>
       <SEO title="PGL" />
       <News />
