1788f53e86751bdb71de326c84b34ac9e585907f
Manuel Brun <manuelbrun@bluewin.ch>
2021-03-22 13:52:52 +0100

    remove logo and correction of Zurück-button

--- src/templates/template-layout.js
+++ src/templates/template-layout.js
@@ -18,14 +18,11 @@ const Layout = ({ children }) => {
         <div className="relative container mx-auto mt-20 h-72 border-b border-pgl-blue">
           <button
             type="button"
-            onClick={() => navigate('/')}
-            className="absolute left-0 bottom-4 items-center ml-4 md:ml-0 px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-pgl-blue hover:bg-blue-800"
+            onClick={() => goBack('B')}
+            className="absolute top-0 right-0 items-center ml-4 md:ml-0 px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-pgl-blue hover:bg-blue-800"
           >
             Zurück
           </button>
-          <div className="absolute top-0 right-0">
-            <StaticImage src="../images/pgl-logo-60.png" alt="pgl-logor" layout="constrained" />
-          </div>
         </div>
         <Content>{children}</Content>
       </PageContainer>
