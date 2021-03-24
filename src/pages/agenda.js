23ebeda7a686104a50826aedf6149727cbbe59dc
Manuel Brun <manuelbrun@bluewin.ch>
2021-03-24 22:04:49 +0100

    update agenda

--- src/pages/agenda.js
+++ src/pages/agenda.js
@@ -10,8 +10,33 @@ const Agenda = () => {
   return (
     <BlueBackground id="agenda" title="Agenda">
       <EventContainer>
-        <Event>Zur Zeit sind keine Anlässe geplant.</Event>
-      </EventContainer>
+            <Event>1. Matinéekonzert</Event>
+            <Datum>am 12.02.2022 um 09:45</Datum>
+            <Location>@ Stadtkeller Luzern</Location>
+          </EventContainer>
+          <EventContainer>
+            <Event>Offene Probe</Event>
+            <Datum>am 17.02.2022 um 19:45</Datum>
+          </EventContainer>
+          <EventContainer>
+            <Event>2. Matinéekonzert</Event>
+            <Datum>am 19.02.2022 um 09:45</Datum>
+            <Location>@ Stadtkeller Luzern</Location>
+          </EventContainer>
+          <EventContainer>
+            <Event>Schmutziger Donnerstag</Event>
+            <Datum>am 24.02.2022</Datum>
+            <Location>@ Luzern</Location>
+          </EventContainer>
+          <EventContainer>
+            <Event>Chöbusamschtig</Event>
+            <Datum>am 26.02.2022 um 18:00</Datum>
+            <Location>@ Stadtkeller Luzern</Location>
+          </EventContainer>
+          <EventContainer>
+            <Event>PGL - 60 Jahre Jubiläumsabend</Event>
+            <Datum>im Frühjahr 2022</Datum>
+          </EventContainer>
     </BlueBackground>
   );
 };
