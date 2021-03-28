2c0933775676d759b76734f281307dca1a4caa79
Patrick Ineichen <github@frokonet.ch>
2021-03-28 06:08:42 +0200

    style: 💄 Center hero logo

--- src/pages/hero.js
+++ src/pages/hero.js
@@ -6,20 +6,23 @@ import { Parallax } from '../components/styles';
 
 const Hero = () => {
   const FullScreenContainer = tw.div`relative h-screen bg-black bg-opacity-40`;
-  const SubTitleContainer = tw.div`relative top-20 text-center`;
-  const SubTitle = tw.h2`text-2xl md:text-6xl font-bold text-gray-100 mb-8`;
-  const ImageContainer = tw.div`absolute bottom-0 md:bottom-4 right-2 md:right-8 ml-2`;
+  const ImageContainer = tw.div`absolute w-screen bottom-20 text-center`;
+  const SubTitle = tw.h2`text-2xl md:text-3xl font-bold text-gray-100 mb-2`;
 
   return (
     <>
       <Parallax>
         <FullScreenContainer>
-          <SubTitleContainer>
+          <ImageContainer>
+            <StaticImage
+              src="../images/hero.png"
+              alt="lettering-header"
+              layout="constrained"
+              width={400}
+              className="mx-4"
+            />
             <SubTitle>Tradition. Schunkeln. Röcke.</SubTitle>
             <SubTitle>Seit 1961.</SubTitle>
-          </SubTitleContainer>
-          <ImageContainer>
-            <StaticImage src="../images/hero.png" alt="lettering-header" layout="constrained" width={580} />
           </ImageContainer>
         </FullScreenContainer>
       </Parallax>
