9b0bf50432392ec5d7b1ca4fa5dd7fd162057642
Patrick Ineichen <github@frokonet.ch>
2021-04-11 18:09:40 +0200

    style: 💄 Info

--- src/sections/info.js
+++ src/sections/info.js
@@ -7,16 +7,16 @@ import { BlueBackground } from '../components/styles';
 const Info = () => {
   const Flex = tw.div`flex`;
   const FlexGrow = tw.div`flex-1`;
-  const Text = tw.p`text-white text-center font-bold mx-2 md:text-2xl`;
+  const Text = tw.p`text-white text-center font-bold mx-2 md:text-4xl`;
 
   return (
     <BlueBackground>
       <Flex>
-        <StaticImage src="../images/jubilaeum.jpeg" alt="jubilaeum" width={150} className="rounded-full ml-2" />
+        <StaticImage src="../images/jubilaeum.jpeg" alt="jubilaeum" width={180} className="rounded-full ml-2" />
         <FlexGrow />
         <Text>Wir jubilieren! 60 Jahre Pilatusgeister Luzern!</Text>
         <FlexGrow />
-        <StaticImage src="../images/jubilaeum.jpeg" alt="jubilaeum" width={150} className="rounded-full mr-2" />
+        <StaticImage src="../images/jubilaeum.jpeg" alt="jubilaeum" width={180} className="rounded-full mr-2" />
       </Flex>
     </BlueBackground>
   );
