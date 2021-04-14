203bf9aa4850608cbfc57d7fbd0c85ae9a493f30
Patrick Ineichen <github@frokonet.ch>
2021-04-14 16:51:39 +0200

    style: 💄 About

--- src/sections/about.js
+++ src/sections/about.js
@@ -6,10 +6,10 @@ import styled from '@emotion/styled';
 
 import { WhiteBackground } from '../components/styles';
 
-const Title = tw.h3`text-pgl-blue text-xl font-bold text-center border-b border-pgl-blue mx-2 mb-2`;
+const Title = tw.h3`text-pgl-blue text-lg lg:text-xl font-bold text-center border-b border-pgl-blue mx-2 mb-2`;
+const Background = tw.div`bg-white mt-4 px-2 lg: px-4 py-4 mx-2 md:mx-0`;
 
 const FliesstextMitSpalten = ({ title, children }) => {
-  const Background = tw.div`bg-white mt-4 px-2 py-4 mx-2 md:mx-0`;
   const Paragraph = styled.p`
     background-color: white;
     padding: 0.5rem;
@@ -39,8 +39,30 @@ const FliesstextMitSpalten = ({ title, children }) => {
 };
 
 const Fliesstext = ({ title, children }) => {
-  const Background = tw.div`bg-white mt-4 px-2 md:px-4 py-4 mx-2 md:mx-0`;
   const Paragraph = styled.p``;
+
+  return (
+    <Background>
+      <Title>{title}</Title>
+      <Paragraph>{children}</Paragraph>
+    </Background>
+  );
+};
+
+const GoetticlubMitglieder = ({ title, children }) => {
+  const Paragraph = styled.p`
+    background-color: white;
+    column-count: 2;
+    column-gap: 10px;
+    margin: 0;
+
+    @media (min-width: 1280px) {
+      column-count: 3;
+      column-gap: 40px;
+      margin: 0.5rem;
+      padding: 0.5rem;
+    }
+  `;
 
   return (
     <Background>
@@ -102,6 +124,10 @@ const byName = (a, b) => {
 
 const About = () => {
   const Title = tw.h1`text-4xl text-center font-bold text-pgl-blue mt-8`;
+  const Absatz = tw.p`mb-4`;
+  const Text = tw.p``;
+  const List = tw.ul`list-disc ml-6`;
+  const ListItem = tw.li`font-bold text-xs lg:text-base`;
 
   const data = useStaticQuery(graphql`
     query MitgliederQuery {
@@ -135,54 +161,52 @@ const About = () => {
   return (
     <WhiteBackground id="about" title="Über uns">
       <FliesstextMitSpalten title="Fasnacht im Herzen. Seit 1961.">
-        Seit 1961 sind die Pilatusgeister fester Bestandteil der Lozärner Fasnacht. Und wo immer sie sind, ist
-        Schunkelstimmung angesagt. Die Klänge der PGL, so das Kürzel der Pilatusgeister Luzern, zielen mitten ins Herz.
-        Melodien von Evergreens, Oberkrainer- und Tessinermusik, südamerikanische Rhythmen, klassische Ohrwürmer,
-        aktuelle Hits und ab und zu ein figulanter Marsch füllen den musikalischen Köcher der PGL. Viele dieser Titel
-        sind auf Tonträgern verewigt. Ob Polka oder Tango, Walzer oder Sirtaki – Vielfalt hat bei den Pilatusgeistern
-        Tradition. Und manch anderes auch noch. <br /> <br /> Die PGL-Matinées, die jeweils an den beiden Samstagen vor
-        der Fasnacht im Stadtkeller über die Bühne gehen, sind legendär. Nicht wenige der Besucher*innen stehen schon
-        morgens um fünf Uhr (!) vor der Stadtkeller-Tür, um sich einen guten Platz zu sichern. Ebenfalls im Stadtkeller
-        findet der von den Pilatusgeistern initiierte und organisierte Chöbu-Samschtig statt. Seit Jahren ist dieser
-        Anlass fester Bestandteil in der Agenda eingefleischter PGL-Fans. Ein ganz besonderes PGL-Schmankerl ist für
-        Insider die offene Probe in der Bocciahalle des FC Kickers. Am letzten Probemittwoch vor der Fasnacht sagen die
-        PGL mit diesem Anlass ihrem Gastgeber Dankeschön. Die Halle ist dabei jeweils zum Bersten voll. <br /> <br /> So
-        traditionsreich wie die PGL-Anlässe sind, so ursprünglich zeigt sich eine der ältesten Guggenmusigen Luzerns in
-        ihrem Auftritt. Die Sujets werden alljährlich von befreundeten Künstlern entworfen. Beim Basteln legen die
-        Mitglieder selbst Hand an. Traditionell kommt die reine Männermusig auch immer im klassischen Gewand mit Rock
-        daher. Ab und zu hört man munkeln, dies werde nur so beibehalten, weil die Röcke so schön schwingen, wenn die
-        Pilatusgeister in ihrem typischen Schunkelschritt daherkommen. Doch wer die PGL kennt, weiss: Die können einfach
-        nicht anders. Denn wenn sie einmal zu marschieren beginnen, dann schlagen die Herzen der begeisterten Mitglieder
-        meistens im Dreivierteltakt. Und jene der vielen Zuhörerinen und Zuhörer meistens etwas höher. Und überhaupt:
-        Die PGL und ihre Fans mögen, lieben und pflegen die Harmonie. Dass dem so ist, beweist auch die Tatsache, dass
-        die Pilatusgeister in ihrer 60-jährigen Vereinsgeschichte erst den dritten Präsidenten haben.
+        <Absatz>
+          Seit 1961 sind die Pilatusgeister fester Bestandteil der Lozärner Fasnacht. Und wo immer sie sind, ist
+          Schunkelstimmung angesagt. Die Klänge der PGL, so das Kürzel der Pilatusgeister Luzern, zielen mitten ins
+          Herz. Melodien von Evergreens, Oberkrainer- und Tessinermusik, südamerikanische Rhythmen, klassische
+          Ohrwürmer, aktuelle Hits und ab und zu ein figulanter Marsch füllen den musikalischen Köcher der PGL. Viele
+          dieser Titel sind auf Tonträgern verewigt. Ob Polka oder Tango, Walzer oder Sirtaki – Vielfalt hat bei den
+          Pilatusgeistern Tradition. Und manch anderes auch noch.
+        </Absatz>
+        <Absatz>
+          Die PGL-Matinées, die jeweils an den beiden Samstagen vor der Fasnacht im Stadtkeller über die Bühne gehen,
+          sind legendär. Nicht wenige der Besucher*innen stehen schon morgens um fünf Uhr (!) vor der Stadtkeller-Tür,
+          um sich einen guten Platz zu sichern. Ebenfalls im Stadtkeller findet der von den Pilatusgeistern initiierte
+          und organisierte Chöbu-Samschtig statt. Seit Jahren ist dieser Anlass fester Bestandteil in der Agenda
+          eingefleischter PGL-Fans. Ein ganz besonderes PGL-Schmankerl ist für Insider die offene Probe in der
+          Bocciahalle des FC Kickers. Am letzten Probemittwoch vor der Fasnacht sagen die PGL mit diesem Anlass ihrem
+          Gastgeber Dankeschön. Die Halle ist dabei jeweils zum Bersten voll.
+        </Absatz>
+        <Absatz>
+          So traditionsreich wie die PGL-Anlässe sind, so ursprünglich zeigt sich eine der ältesten Guggenmusigen
+          Luzerns in ihrem Auftritt. Die Sujets werden alljährlich von befreundeten Künstlern entworfen. Beim Basteln
+          legen die Mitglieder selbst Hand an. Traditionell kommt die reine Männermusig auch immer im klassischen Gewand
+          mit Rock daher. Ab und zu hört man munkeln, dies werde nur so beibehalten, weil die Röcke so schön schwingen,
+          wenn die Pilatusgeister in ihrem typischen Schunkelschritt daherkommen. Doch wer die PGL kennt, weiss: Die
+          können einfach nicht anders. Denn wenn sie einmal zu marschieren beginnen, dann schlagen die Herzen der
+          begeisterten Mitglieder meistens im Dreivierteltakt. Und jene der vielen Zuhörerinen und Zuhörer meistens
+          etwas höher. Und überhaupt: Die PGL und ihre Fans mögen, lieben und pflegen die Harmonie. Dass dem so ist,
+          beweist auch die Tatsache, dass die Pilatusgeister in ihrer 60-jährigen Vereinsgeschichte erst den dritten
+          Präsidenten haben.
+        </Absatz>
       </FliesstextMitSpalten>
 
       <Fliesstext title="PGL Facts in Kürze">
-        <p>Die Pilatusgeister sind eine der wenigen klassischen Männermusigen Luzerns.</p>
-        <p>
+        <Text>Die Pilatusgeister sind eine der wenigen klassischen Männermusigen Luzerns.</Text>
+        <Text>
           Gegründet wurden die Pilatusgeister Luzern 1961 von Heinz Gloor, Sigi Widmer, Noldi Torricelli und Fritz
           Winteler.
-        </p>
-        <p>Wir sind Mitglied der "Vereinigte" seit deren Gründung im Jahr 1964.</p>
-        <p>Der aktuelle Vorstand besteht aus</p>
-        <ul>
-          <li>
-            <b>Beat Thalmann - Präsident,</b>
-          </li>
-          <li>
-            <b>Andi Felber - Tambourmajor und Vize-Präsident,</b>
-          </li>
-          <li>
-            <b>Markus Helfenstein - Kassier,</b>
-          </li>
-          <li>
-            <b>Manuel Brun - Beisitzer,</b>
-          </li>
-          <li>
-            <b>René Sidler - Aktuar</b>
-          </li>
-        </ul>
+        </Text>
+        <Text>Wir sind Mitglied der "Vereinigte" seit deren Gründung im Jahr 1964.</Text>
+        <Text>Der aktuelle Vorstand besteht aus:</Text>
+        <List>
+          <ListItem>Beat Thalmann - Präsident</ListItem>
+          <ListItem>Andi Felber - Tambourmajor und Vize-Präsident</ListItem>
+          <ListItem>Markus Helfenstein - Kassier</ListItem>
+          <ListItem>Manuel Brun - Beisitzer</ListItem>
+          <ListItem>René Sidler - Aktuar</ListItem>
+        </List>
       </Fliesstext>
 
       <Title>Aktivmitglieder</Title>
@@ -210,14 +234,44 @@ const About = () => {
         guggenmusikalische hinausgeht.
       </FliesstextMitSpalten>
 
-      <FliesstextMitSpalten title="Götticlub-Mitglieder (nach Eintrittsjahr)">
-        Rolf Albisser, Renato Amado, Erwin "Wini" Bächler, Peter Bachmann, Turi Balzarini, Walter Burckhard (†), Peter
-        Fleischli, Erich Hunkeler, Remo Moor, Hans Odermatt, Anton A. Oetterli (†), Hugo "Stöge" Stocker, Arnold "Noldi"
-        Torricelli, Charles "Charly" Vögeli (†), Fritz Winteler, Hans-Ruedi Zai (Fahnengötti), Bruno Piemontesi, Beat
-        Stauffer, Tobias Widmer, Renato Bertelle (†), Armin "Büsu" Suppiger, Thomas Träger, Ceo Benedetti, Christoph
-        Träger, Hansruedi Jossi, Beat "Möpsu" Kunz, Albert "Bärti" Felber (†), Andy Bühlmann, Kurt Maurer, Othmar
-        Schütz, Hans Fleischli, Patrick Kunz, Daniel Renggli, Daniel Haas
-      </FliesstextMitSpalten>
+      <GoetticlubMitglieder title="Götticlub-Mitglieder (nach Eintrittsjahr)">
+        <List>
+          <ListItem>Rolf Albisser</ListItem>
+          <ListItem>Renato Amado</ListItem>
+          <ListItem>Erwin "Wini" Bächler</ListItem>
+          <ListItem>Peter Bachmann</ListItem>
+          <ListItem>Turi Balzarini</ListItem>
+          <ListItem>Walter Burckhard (†)</ListItem>
+          <ListItem> Peter Fleischli</ListItem>
+          <ListItem>Erich Hunkeler</ListItem>
+          <ListItem>Remo Moor</ListItem>
+          <ListItem>Hans Odermatt</ListItem>
+          <ListItem>Anton A. Oetterli (†)</ListItem>
+          <ListItem>Hugo "Stöge" Stocker</ListItem>
+          <ListItem>Arnold "Noldi" Torricelli</ListItem>
+          <ListItem>Charles "Charly" Vögeli (†)</ListItem>
+          <ListItem>Fritz Winteler</ListItem>
+          <ListItem>Hans-Ruedi Zai (Fahnengötti)</ListItem>
+          <ListItem>Bruno Piemontesi</ListItem>
+          <ListItem>Beat Stauffer</ListItem>
+          <ListItem>Tobias Widmer</ListItem>
+          <ListItem>Renato Bertelle (†)</ListItem>
+          <ListItem>Armin "Büsu" Suppiger</ListItem>
+          <ListItem>Thomas Träger</ListItem>
+          <ListItem>Ceo Benedetti</ListItem>
+          <ListItem>Christoph Träger</ListItem>
+          <ListItem>Hansruedi Jossi</ListItem>
+          <ListItem>Beat "Möpsu" Kunz</ListItem>
+          <ListItem>Albert "Bärti" Felber (†)</ListItem>
+          <ListItem>Andy Bühlmann</ListItem>
+          <ListItem>Kurt Maurer</ListItem>
+          <ListItem>Othmar Schütz</ListItem>
+          <ListItem>Hans Fleischli</ListItem>
+          <ListItem>Patrick Kunz</ListItem>
+          <ListItem>Daniel Renggli</ListItem>
+          <ListItem>Daniel Haas</ListItem>
+        </List>
+      </GoetticlubMitglieder>
     </WhiteBackground>
   );
 };
