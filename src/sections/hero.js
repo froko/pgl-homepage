19bbf7c750d7560f5938d1126fe961d11f2acc1a
Patrick Ineichen <github@frokonet.ch>
2021-04-19 07:10:39 +0200

    style: 💄 AdBanner height

--- src/sections/hero.js
+++ src/sections/hero.js
@@ -7,7 +7,7 @@ import AdBanner from '../components/ad-banner';
 
 const Hero = () => {
   const FullScreenContainer = tw.div`relative h-screen bg-black bg-opacity-40`;
-  const ImageContainer = tw.div`absolute w-screen bottom-32 lg:bottom-20 text-center`;
+  const ImageContainer = tw.div`absolute w-screen bottom-36 lg:bottom-20 text-center`;
   const AdContainer = tw.div`absolute w-screen bottom-0 h-32 lg:h-16`;
   const SubTitle = tw.h2`text-3xl lg:text-4xl font-bold text-gray-100 mb-2`;
 
