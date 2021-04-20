c8651e28e719b493c26ef063d215850dcb107624
Patrick Ineichen <github@frokonet.ch>
2021-04-20 11:04:56 +0200

    feat: 🎸 Add Schubiger Ad

--- src/components/ad-banner.js
+++ src/components/ad-banner.js
@@ -6,7 +6,7 @@ import tw from 'twin.macro';
 const AdBanner = () => {
   const WhiteBackground = tw.div`w-full bg-white`;
   const FlexBox = tw.div`flex flex-wrap mx-2 justify-center`;
-  const ImageContainer = tw.div`py-2 w-1/3 lg:w-1/5`;
+  const ImageContainer = tw.div`py-2 w-1/2 lg:w-1/5`;
 
   const quality = 90;
   const height = 44;
@@ -27,6 +27,17 @@ const AdBanner = () => {
           </Link>
         </ImageContainer>
         <ImageContainer>
+          <Link to="http://www.hotel-waldstaetterhof.ch/">
+            <StaticImage
+              src="../images/ads/waldstaetterhof.png"
+              alt="waldstaetterhof"
+              quality={quality}
+              height={height}
+              placeholder={placeholder}
+            />
+          </Link>
+        </ImageContainer>
+        <ImageContainer>
           <Link to="https://www.motelbrueggli.ch/">
             <StaticImage
               src="../images/ads/brueggli.png"
@@ -37,11 +48,12 @@ const AdBanner = () => {
             />
           </Link>
         </ImageContainer>
+
         <ImageContainer>
-          <Link to="http://www.hotel-waldstaetterhof.ch/">
+          <Link to="https://www.schubiger-nw.ch/">
             <StaticImage
-              src="../images/ads/waldstaetterhof.png"
-              alt="waldstaetterhof"
+              src="../images/ads/schubiger.png"
+              alt="schubiger"
               quality={quality}
               height={height}
               placeholder={placeholder}
