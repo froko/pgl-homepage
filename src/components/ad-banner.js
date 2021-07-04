7fc4c6363e50e3f55ffbab56328b50126bde1a53
Patrick Ineichen <patrick.ineichen@bbv.ch>
2021-07-04 19:02:26 +0200

    feat: 🎸 Ads
    Add Zurich ad and improve placement

--- src/components/ad-banner.js
+++ src/components/ad-banner.js
@@ -5,38 +5,17 @@ import tw from 'twin.macro';
 const AdBanner = () => {
   const WhiteBackground = tw.div`w-full bg-white`;
   const FlexBox = tw.div`flex flex-wrap mx-2 justify-center text-center`;
-  const ImageContainer = tw.div`py-2 w-1/2 lg:w-1/5`;
+  const ImageContainer = tw.div`py-1 w-1/3 lg:w-1/5`;
+  const Placeholder = tw(ImageContainer)`block lg:hidden`;
   const Link = tw.a``;
 
   const quality = 90;
-  const height = 44;
+  const height = 56;
   const placeholder = 'tracedSVG';
 
   return (
     <WhiteBackground>
       <FlexBox>
-        <ImageContainer>
-          <Link href="https://www.stadtkeller.ch/">
-            <StaticImage
-              src="../images/ads/stadtkeller.png"
-              alt="stadtkeller"
-              quality={quality}
-              height={height}
-              placeholder={placeholder}
-            />
-          </Link>
-        </ImageContainer>
-        <ImageContainer>
-          <Link href="http://www.hotel-waldstaetterhof.ch/">
-            <StaticImage
-              src="../images/ads/waldstaetterhof.png"
-              alt="waldstaetterhof"
-              quality={quality}
-              height={height}
-              placeholder={placeholder}
-            />
-          </Link>
-        </ImageContainer>
         <ImageContainer>
           <Link href="https://www.motelbrueggli.ch/">
             <StaticImage
@@ -48,11 +27,49 @@ const AdBanner = () => {
             />
           </Link>
         </ImageContainer>
+        <Placeholder />
+        <ImageContainer>
+          <Link href="http://www.hotel-waldstaetterhof.ch/">
+            <StaticImage
+              src="../images/ads/waldstaetterhof.png"
+              alt="waldstaetterhof"
+              quality={quality}
+              height={height}
+              placeholder={placeholder}
+            />
+          </Link>
+        </ImageContainer>
+
+        <Placeholder />
+        <ImageContainer>
+          <Link href="https://www.stadtkeller.ch/">
+            <StaticImage
+              src="../images/ads/stadtkeller.png"
+              alt="stadtkeller"
+              quality={quality}
+              height={height}
+              placeholder={placeholder}
+            />
+          </Link>
+        </ImageContainer>
+        <Placeholder />
         <ImageContainer>
           <Link href="https://www.schubiger-nw.ch/">
             <StaticImage
               src="../images/ads/schubiger.png"
               alt="schubiger"
+              quality={quality}
+              height={height}
+              placeholder={placeholder}
+            />
+          </Link>
+        </ImageContainer>
+        <Placeholder />
+        <ImageContainer>
+          <Link href="https://zurich.ch/mani">
+            <StaticImage
+              src="../images/ads/zurich.png"
+              alt="zurich"
               quality={quality}
               height={height}
               placeholder={placeholder}
--- src/sections/hero.js
+++ src/sections/hero.js
@@ -6,7 +6,7 @@ import { Parallax } from '../components/styles';
 import AdBanner from '../components/ad-banner';
 
 const Hero = () => {
-  const FullScreenContainer = tw.div`relative h-screen bg-black bg-opacity-40`;
+  const FullScreenContainer = tw.div`relative h-screen bg-black bg-opacity-30`;
   const ImageContainer = tw.div`absolute w-screen bottom-44 lg:bottom-24 text-center`;
   const AdContainer = tw.div`absolute w-screen bottom-8 lg:bottom-0 h-36 lg:h-16`;
   const SubTitle = tw.h2`text-3xl lg:text-4xl font-bold text-gray-100 mb-2`;
@@ -19,7 +19,7 @@ const Hero = () => {
             <StaticImage
               src="../images/hero.png"
               alt="lettering-header"
-              width={340}
+              width={330}
               className="mx-4"
               placeholder="tracedSVG"
             />
